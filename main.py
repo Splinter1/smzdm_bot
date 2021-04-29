233434ee430205735f7492712da58c5733f4cb14
lizongzheng <574222018@qq.com>
2021-04-29 11:23:23 +0800

    推送到手机brak

--- main.py
+++ main.py
@@ -56,11 +56,14 @@ if __name__ == '__main__':
     sb.load_cookie_str(cookies)
     res = sb.checkin()
     print(res)
-    SERVERCHAN_SECRETKEY = os.environ["SERVERCHAN_SECRETKEY"]
-    print('sc_key: ', SERVERCHAN_SECRETKEY)
-    if isinstance(SERVERCHAN_SECRETKEY,str) and len(SERVERCHAN_SECRETKEY)>0:
-        print('检测到 SCKEY， 准备推送')
-        push_to_wechat(text = '什么值得买每日签到',
-                        desp = str(res),
-                        secretKey = SERVERCHAN_SECRETKEY)
+    # SERVERCHAN_SECRETKEY = os.environ["SERVERCHAN_SECRETKEY"]
+    # print('sc_key: ', SERVERCHAN_SECRETKEY)
+    # if isinstance(SERVERCHAN_SECRETKEY,str) and len(SERVERCHAN_SECRETKEY)>0:
+    #     print('检测到 SCKEY， 准备推送')
+    #     push_to_wechat(text = '什么值得买每日签到',
+    #                     desp = str(res),
+    #                     secretKey = SERVERCHAN_SECRETKEY)
+    url = 'https://api.day.app/6FKe3WfetAuTPVduNYqzpN/已经打卡咯'
+    msg = requests.get(url)
+    print(msg)
     print('代码完毕')
